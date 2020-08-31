7724e26be09c723f0da7d9eb65d8291dca85ceac
Rob Bikmansurov <robb@mail.ru>
2020-08-31 21:35:55 +0500

    add bootstrap, fix navbar styles in layout

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,13 +1,23 @@
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
+// a relevant structure within app/frontend and only use these pack files to reference
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
+require("@rails/activestorage").start()
+require("bootstrap/dist/js/bootstrap");
+
+var jQuery = require("jquery");
+// import jQuery from "jquery";
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
+
+import '../styles/application'
+
+const images = require.context('../../assets/images', true)
 // const imagePath = (name) => images(name, true)
