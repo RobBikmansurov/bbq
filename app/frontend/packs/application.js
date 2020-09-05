f2a685cc06afe75e77037812f8f7904c99fd83a8
Rob Bikmansurov <robb@mail.ru>
2020-09-05 15:38:22 +0500

    try to configure webpacker, but background-image is not showing

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -4,20 +4,23 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("@rails/activestorage").start()
-require("bootstrap/dist/js/bootstrap")
 
 var jQuery = require("jquery");
 // import jQuery from "jquery";
 global.$ = global.jQuery = jQuery;
 window.$ = window.jQuery = jQuery;
 
+
+import "bootstrap"
+
+import '../stylesheets/application'
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-
-import '../styles/application'
-
-const images = require.context('../../assets/images', true)
+// const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
