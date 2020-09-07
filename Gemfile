c34cf41ebf3f0305ca9eafad9f4c4cf6cd16f0cf
Rob Bikmansurov <robb@mail.ru>
2020-09-07 21:51:40 +0500

    fix error with user sign_out - method DELETE became GET

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ ruby '2.7.1'
 
 gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
 gem 'puma', '~> 4.1'
-gem 'webpacker'
+gem 'webpacker', '~> 4.0'
 
 gem 'devise'
 gem 'devise-i18n'
@@ -13,12 +13,11 @@ gem 'rails-i18n', '~> 6.0.0'
 gem 'russian', '~> 0.6.0'
 
 group :development, :test do
-  gem 'sqlite3', '~> 1.4'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'sqlite3', '~> 1.4'
 end
 
 group :development do
-  gem 'listen'
   gem 'web-console', '>= 3.3.0'
 end
 
--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,23 +1,19 @@
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/frontend and only use these pack files to reference
+// a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
 
-var jQuery = require("jquery");
-// import jQuery from "jquery";
-global.$ = global.jQuery = jQuery;
-window.$ = window.jQuery = jQuery;
-
-import "bootstrap"
-
-import '../stylesheets/application'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-
 const images = require.context('../images', true)
-const imagePath = (name) => images(name, true)
+// const imagePath = (name) => images(name, true)
+
+import './bootstrap_custom'
+
+import '../stylesheets/application'
+
