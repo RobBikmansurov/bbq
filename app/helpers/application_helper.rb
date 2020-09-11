8eace46b05c8de09d5332177d3cbb4d37547bcf9
Rob Bikmansurov <robb@mail.ru>
2020-09-11 21:05:17 +0500

    already subscribed user cannot subscribe to the event

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,12 +7,17 @@ module ApplicationHelper
     user_signed_in? && (
     model.user == current_user ||
       (model.try(:event).present? && model.event.user == current_user)
-  )
+    )
   end
 
   def current_user_can_create?(model)
-    user_signed_in? &&
-      model.try(:event).present? &&
-      model.event.user != current_user
+    return true unless user_signed_in?
+
+    model.try(:event).present? && model.event.user != current_user
+  end
+
+  def current_user_already_subscribed?(model)
+    event = model.try(:event)
+    user_signed_in? && event.present? && event.subscriptions.where(user_id: current_user.id).any?
   end
 end
