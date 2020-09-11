b4f62e52f82385b173a55325887a33e55683aa8a
Rob Bikmansurov <robb@mail.ru>
2020-09-11 21:28:54 +0500

    improve code: check event subscriptions count to show who is going, move some functions to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user_can_edit?
+  helper_method :current_user_can_create?
+  helper_method :current_user_already_subscribed?
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -10,7 +12,21 @@ class ApplicationController < ActionController::Base
     )
   end
 
-  def current_user_can_edit?(event)
-    user_signed_in? && event.user == current_user
+  def current_user_can_edit?(model)
+    user_signed_in? && (
+    model.user == current_user ||
+      (model.try(:event).present? && model.event.user == current_user)
+    )
+  end
+
+  def current_user_can_create?(model)
+    return true unless user_signed_in?
+
+    model.try(:event).present? && model.event.user != current_user
+  end
+
+  def current_user_already_subscribed?(model)
+    event = model.try(:event)
+    user_signed_in? && event.present? && event.subscriptions.where(user_id: current_user.id).any?
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,22 +2,4 @@ module ApplicationHelper
   def user_avatar(_user)
     'user.png'
   end
-
-  def current_user_can_edit?(model)
-    user_signed_in? && (
-    model.user == current_user ||
-      (model.try(:event).present? && model.event.user == current_user)
-    )
-  end
-
-  def current_user_can_create?(model)
-    return true unless user_signed_in?
-
-    model.try(:event).present? && model.event.user != current_user
-  end
-
-  def current_user_already_subscribed?(model)
-    event = model.try(:event)
-    user_signed_in? && event.present? && event.subscriptions.where(user_id: current_user.id).any?
-  end
 end
