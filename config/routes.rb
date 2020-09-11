5fab775ab0a9b5c5369b3ffb8590555536f2d94f
Rob Bikmansurov <robb@mail.ru>
2020-09-11 16:29:57 +0500

    add comments for events

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :events
+  resources :events do
+    resources :comments, only: [:create, :destroy]
+  end
   resources :users, only: %i[show edit update]
 
   root to: 'events#index'
