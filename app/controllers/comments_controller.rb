4b837d56ae648846f0235e9cfe2e8bb2e1e974e0
Rob Bikmansurov <robb@mail.ru>
2020-09-11 16:29:57 +0500

    add comments for events

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,43 @@
+class CommentsController < ApplicationController
+  before_action :load_comment, only: :destroy
+  before_action :load_event, only: [:create, :destroy]
+  before_action :load_comment, only: [:destroy]
+
+  def create
+    @comment = @event.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to @event, notice: I18n.t('controllers.comments.created')
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    message = {notice: I18n.t('controllers.comments.destroyed')}
+    if current_user_can_edit?(@comment)
+      @comment.destroy
+    else
+      message = {alert: I18n.t('controllers.comments.error')}
+    end
+    redirect_to @event, message
+  end
+
+  private
+
+  def load_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def load_event
+    @event = Event.find(params[:event_id])
+  end
+
+  def load_comment
+    @comment = @event.comments.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body, :user_name)
+  end
+end
