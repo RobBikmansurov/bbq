b8132dab670254f67ce1e556b77dcaa60892d730
Rob Bikmansurov <robb@mail.ru>
2020-09-14 17:09:53 +0500

    add phtos for events, add avatar on top menu if user logged in

---
+++ app/models/photo.rb
@@ -0,0 +1,11 @@
+class Photo < ApplicationRecord
+  belongs_to :event
+  belongs_to :user
+
+  validates :photo, presence: true
+
+  mount_uploader :photo, PhotoUploader
+
+  scope :persisted, -> { where.not(id: nil) }
+
+end
