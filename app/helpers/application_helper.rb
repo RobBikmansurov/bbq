d7b767c8cfe839de557a299441a62b605c4b0f59
Rob Bikmansurov <robb@mail.ru>
2020-09-14 16:45:52 +0500

    add show avatar thumbnail for user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-  def user_avatar(_user)
-    'user.png'
+  def user_avatar(user)
+    if user.avatar?
+      user.avatar.thumb.url
+    else
+      asset_pack_path('media/images/user.png')
+    end
   end
 end
