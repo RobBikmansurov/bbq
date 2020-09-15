7209fa41698c4a64678ac06c4f0aa66b92729641
Rob Bikmansurov <robb@mail.ru>
2020-09-15 17:32:01 +0500

    add config for Sendgrid in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,10 +48,10 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: '587',
+    address:   'smtp.gmail.com',
+    port:      '587',
     user_name: Rails.application.credentials.send(Rails.env)[:mail][:user_name],
-    password: Rails.application.credentials.send(Rails.env)[:mail][:password],
+    password:  Rails.application.credentials.send(Rails.env)[:mail][:password],
     authentication: 'plain',
     enable_starttls_auto: true
   }
