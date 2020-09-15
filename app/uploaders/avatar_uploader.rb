0709edf9c503d53b216f5a26fbaea8688cb77e94
Rob Bikmansurov <robb@mail.ru>
2020-09-15 12:23:25 +0500

    add aws S3 bucket config to upload photos and avatars

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,9 +1,11 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
