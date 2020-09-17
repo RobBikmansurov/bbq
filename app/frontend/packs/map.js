158f52b855dc238a6df22f929aa6d32b16697627
Rob Bikmansurov <robb@mail.ru>
2020-09-17 18:14:50 +0500

    add yandex-map for event again

--- app/frontend/packs/map.js
+++ app/frontend/packs/map.js
@@ -5,7 +5,28 @@ function init(){
   address = document.getElementById('map').getAttribute('data-address');
 
   myMap = new ymaps.Map("map", {
-      center: [55.76, 37.64],
+      center: [58.010450, 56.229434],
       zoom: 10
   });
+
+  myGeocoder = ymaps.geocode(address);
+
+  myGeocoder.then(
+    function (res) {
+      coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+
+      myMap.geoObjects.add(
+          new ymaps.Placemark(
+            coordinates,
+            {iconContent: address},
+            {preset: 'islands#blueStretchyIcon'}
+          )
+      );
+
+      myMap.setCenter(coordinates);
+      myMap.setZoom(15);
+    }, function (err) {
+      alert('Ошибка при определении местоположения');
+    }
+  );
 }
