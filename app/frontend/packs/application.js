83ea21c5e01f32de2b7b0b1665751fd68f35ba8f
Rob Bikmansurov <robb@mail.ru>
2020-09-17 18:14:50 +0500

    add yandex-map for event again

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start();
-require("air-datepicker")
+require("air-datepicker");
 require("./map");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
