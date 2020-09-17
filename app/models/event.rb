14273b85a50923a16eb6f767221bc6867c64b07a
Rob Bikmansurov <robb@mail.ru>
2020-09-17 13:10:58 +0500

    add pincode to access some event

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,12 +5,16 @@ class Event < ApplicationRecord
   has_many :subscribers, through: :subscriptions, source: :user
   has_many :photos, dependent: :destroy
 
-  validates :title, presence: true, length: { maximum: 255 }
+  validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true
   validates :datetime, presence: true
   validates :user, presence: true
 
   def visitors
     (subscribers + [user]).uniq
+  end
+
+  def pincode_valid?(pin2chek)
+    pincode == pin2chek
   end
 end
