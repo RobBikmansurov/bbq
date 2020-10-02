efb913874252f7cc2afc8eabe3d8d5728b6606c0
Rob Bikmansurov <robb@mail.ru>
2020-10-02 11:58:52 +0500

    set RAILS_ENV for github action, move aws settings to the credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,11 +2,11 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider:              'AWS',
-      region:                ENV['S3_REGION'],
-      aws_access_key_id:     ENV['S3_ACCESS_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET_KEY']
+      region:                Rails.application.credentials.aws[:S3_REGION],
+      aws_access_key_id:     Rails.application.credentials.aws['S3_ACCESS_KEY'],
+      aws_secret_access_key: Rails.application.credentials.aws['S3_SECRET_KEY']
     }
 
-    config.fog_directory  = ENV['S3_BUCKET_NAME']
+    config.fog_directory  = Rails.application.credentials.aws['S3_BUCKET_NAME']
   end
 end
