2b4065ca8ad7edbedd735dc4ab2186fdef7a8f5e
Rob Bikmansurov <robb@mail.ru>
2020-10-05 14:30:07 +0500

    fix indentation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -38,7 +38,7 @@ class CommentsController < ApplicationController
   end
 
   def notify_subscribers(event, comment)
-     subscribers = event.subscriptions.map(&:user_email) - [current_user&.email]
+    subscribers = event.subscriptions.map(&:user_email) - [current_user&.email]
     (subscribers + [event.user.email])
       .uniq
       .each do |mail|
