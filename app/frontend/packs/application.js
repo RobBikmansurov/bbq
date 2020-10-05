e6a45ae0521b7940a4424c933b6a826e32fe15ef
Rob Bikmansurov <robb@mail.ru>
2020-10-05 15:39:28 +0500

    add image gallery for photos, fix photo deletion

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -6,8 +6,9 @@
 require("@rails/ujs").start();
 require("air-datepicker");
 require("./map");
-require("ekko-lightbox");
+require("./ekko-lightbox.min");
 require("./gallery")
+import 'bootstrap/dist/js/bootstrap'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
