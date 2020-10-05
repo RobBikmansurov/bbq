bfb588f200476b031f89e0cc09c3f473b79df519
Rob Bikmansurov <robb@mail.ru>
2020-10-05 14:31:12 +0500

    fix js error on main page

--- app/frontend/packs/map.js
+++ app/frontend/packs/map.js
@@ -2,31 +2,34 @@ ymaps.ready(init);
 var myMap;
 
 function init(){
-  address = document.getElementById('map').getAttribute('data-address');
+  map = document.getElementById('map');
+  if (map) {
+    address = map.getAttribute('data-address');
 
-  myMap = new ymaps.Map("map", {
+    myMap = new ymaps.Map("map", {
       center: [58.010450, 56.229434],
       zoom: 10
-  });
+    });
 
-  myGeocoder = ymaps.geocode(address);
+    myGeocoder = ymaps.geocode(address);
 
-  myGeocoder.then(
-    function (res) {
-      coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+    myGeocoder.then(
+      function (res) {
+        coordinates = res.geoObjects.get(0).geometry.getCoordinates();
 
-      myMap.geoObjects.add(
+        myMap.geoObjects.add(
           new ymaps.Placemark(
             coordinates,
             {iconContent: address},
             {preset: 'islands#blueStretchyIcon'}
           )
-      );
+        );
 
-      myMap.setCenter(coordinates);
-      myMap.setZoom(15);
-    }, function (err) {
-      alert('Ошибка при определении местоположения');
-    }
-  );
+        myMap.setCenter(coordinates);
+        myMap.setZoom(15);
+      }, function (err) {
+        alert('Ошибка при определении местоположения');
+      }
+    );
+  }
 }
