e9c6b92dca6fec6f63b27dafaf17c7973a2b9d47
Rob Bikmansurov <robb@mail.ru>
2020-10-28 11:25:16 +0500

    add resque restart after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -70,5 +70,5 @@ set :bundle_jobs, 4 # default: 4, only available for Bundler >= 1.4
 set :bundle_binstubs, -> { shared_path.join('bin') }
 
 after 'deploy:published', 'bundler:clean' # clean gems after successful deploy
-# after "deploy:restart", "resque:restart"
+after "deploy:restart", "resque:restart"
 
